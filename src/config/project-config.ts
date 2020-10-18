2052dbd26ffcaeb14b6970ee7454267609d249e9
Jan Nowakowski <aw3som3pl@gmail.com>
2020-10-18 14:56:48 +0200

    updated config for dev

--- src/config/project-config.ts
+++ src/config/project-config.ts
@@ -1,13 +1,13 @@
 export const projectConfig = {
   firebase: {
-    apiKey: 'AIzaSyD9q_TpYirkNWFdeBhcFNd47gx4W2NyS4g',
-    authDomain: 'kaizenchallenge-demo.firebaseapp.com',
-    databaseURL: 'https://kaizenchallenge-demo.firebaseio.com',
-    projectId: 'kaizenchallenge-demo',
-    storageBucket: 'kaizenchallenge-demo.appspot.com',
-    messagingSenderId: '38818715791',
-    appId: '1:38818715791:web:451b52b27cc08190feb8fa',
-    measurementId: 'G-D32R4V8NNR'
+    apiKey: 'AIzaSyAF1gInpQlUuI12Iqepd8g72JYjd6nDxyM',
+    authDomain: 'kaizenchallenge-testdev.firebaseapp.com',
+    databaseURL: 'https://kaizenchallenge-testdev.firebaseio.com',
+    projectId: 'kaizenchallenge-testdev',
+    storageBucket: 'kaizenchallenge-testdev.appspot.com',
+    messagingSenderId: '393990589309',
+    appId: '1:393990589309:web:0cf52baeb7b13db85eda14',
+    measurementId: 'G-125VSRF5GF'
   },
   apiBaseUrl: 'https://kaizenonline.azurewebsites.net'
 };
